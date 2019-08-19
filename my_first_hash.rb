8aa0b3ead45cfac3c9545ae28f4950d42bb15a4c
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-19 06:49:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,7 +28,8 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
-                  "musket" => 
+                  "musket" => 2 
+                  "gun powder" => 4
                  }
   # add 2 muskets to the shipping_manifest hash below
 
@@ -39,5 +40,5 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+  return shipping_manifest
 end
